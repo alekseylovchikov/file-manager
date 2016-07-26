2a7dfced74a4842ef3e81df81e0827c8c8b81e90
Aleksey Lovchikov <aleksey.lovchikov@yandex.ru>
2016-07-26 09:31:22 +0000

    add normalize

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
   resources :docs
   
   authenticated :user do
@@ -8,4 +6,8 @@ Rails.application.routes.draw do
   end
   
   root 'welcome#index'
+  
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
